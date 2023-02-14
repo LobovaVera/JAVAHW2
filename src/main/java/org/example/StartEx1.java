45a2ce94e4198f927866f26a9051442423b25d83
vera <king.vera7@gmail.com>
2023-02-14 15:33:34 +0300

    Initial commit

---
+++ src/main/java/org/example/StartEx1.java
@@ -0,0 +1,20 @@
+package org.example;
+import com.sun.tools.javac.Main;
+
+public class StartEx1 {
+    public static void main(String[] args) {
+
+        int[] our_array = MyLib.getSubsequence();
+        int sum = 0;
+        MyLib.printArray(our_array);
+
+        for (int i = 0; i < our_array.length; i++) {
+            if (PrimeNum.primeInt(our_array[i])) {
+                System.out.print(our_array[i] + " ");
+                sum += our_array[i];
+            }
+            System.out.println(PrimeNum.primeInt(our_array[i]));
+        }
+        System.out.println(String.format("Сумма простых чисел массива = %d", sum));
+    }
+}
